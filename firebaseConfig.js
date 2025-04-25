1cacdc638149a0fcf3a8d7691dfcaed60618153a
Brandon <quinta.cobrandon@gmail.com>
2025-04-24 18:51:59 -0600

    aa

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "jaloapp-9de8d",
   storageBucket: "jaloapp-9de8d.appspot.com",
   messagingSenderId: "138019762076",
-  appId: "1:138019762076:web:d15ce8de5539858ab7e259",
+      appId: "1:138019762076:web:d15ce8de5539858ab7e259",
   measurementId: "G-BEPTTQTGVF",
 };
 
